2185ca936ba583be0038a978d27a3d66fabcdb21
WilliamMcRoberts <wamcroberts2@gmail.com>
2022-11-20 18:33:35 -0500

    initial

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -1,5 +1,45 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  stable var currentValue: Float = 0;
+  // currentValue := 0;
+  let id = 546757574357;
+
+  stable var startTime = Time.now();
+  // startTime := Time.now();
+  Debug.print(debug_show(startTime));
+
+  // Update call
+  public func topUp(amount : Float) {
+    currentValue += amount;
+    Debug.print(debug_show(currentValue));
   };
-};
+
+  // Update call
+  public func withdraw(amount : Float) {
+    let tempValue: Float = currentValue - amount;
+    if(tempValue < 0){
+      Debug.print("There are not enough funds for that size of a withdraw");
+      return; 
+      };
+    currentValue -= amount;
+    Debug.print(debug_show(currentValue));
+  };
+
+  // Update call
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+    startTime := currentTime;
+  };
+
+  // Query call
+  public query func checkBalance() : async Float {
+    return currentValue;
+  };
+
+}
--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -1,19 +1,60 @@
+
 import { dbank_backend } from "../../declarations/dbank_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+window.addEventListener("load", async function() {
+  await queryBalance();
+});
 
-  const name = document.getElementById("name").value.toString();
+document.querySelector("#deposit").addEventListener("submit", async function(event) {
+  event.preventDefault();
+  
+  const button = event.target.querySelector("#submit-btn");
+
+  if(document.getElementById("input-amount").value.length == 0 || parseFloat(document.getElementById("input-amount").value) <= 0) {
+    console.log("Amount must have a value more than zero");
+    return;
+  }
 
   button.setAttribute("disabled", true);
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank_backend.greet(name);
+  await dbank_backend.topUp(inputAmount);
 
+  await queryBalance();
+
+  document.getElementById("input-amount").value = "";
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
 });
+
+
+document.querySelector("#submit-btn-withdraw").addEventListener("click", async function(event) {
+  event.preventDefault();
+  
+  const button = event.target.querySelector("#submit-btn-withdraw");
+
+  if(document.getElementById("withdraw-amount").value.length == 0 || parseFloat(document.getElementById("withdraw-amount").value) <= 0) {
+    console.log("Amount must have a value more than zero");
+    return;
+  }
+
+  const outputAmount = parseFloat(document.getElementById("withdraw-amount").value);
+
+  await dbank_backend.withdraw(outputAmount);
+
+  await queryBalance();
+  
+  document.getElementById("withdraw-amount").value = "";
+});
+
+document.querySelector("#compound-btn").addEventListener("click", async function(event) {
+  event.preventDefault();
+  
+  dbank_backend.compound();
+
+  await queryBalance();
+});
+
+async function queryBalance() {
+  const currentAmount = await dbank_backend.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+}
